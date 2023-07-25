25c921b8f988bb0bb214cca06cb16dc0a5ab6c3c
22bca02@vtcbcsr.edu.in <128131333+amit-sharmaA@users.noreply.github.com>
2023-07-25 12:07:41 +0530

    Update P5 SOLUTION.py

--- Week1/P5 SOLUTION.py
+++ Week1/P5 SOLUTION.py
@@ -3,7 +3,7 @@ c = 0
 
 A = A.lower()
 for i in A:
-    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'or i == 'A' or i == 'E' or i == 'I' or i == 'O' or i == 'U':
     
         c+=1
 
